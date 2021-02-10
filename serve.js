336c91b59dfd19d41fdd2ab59ba788777f08cc3f
tg-z <parasitism@protonmail.com>
2021-02-09 18:38:59 -0800

    added img to static folder

--- serve.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const router = express.Router();
-app.use(bodyParser.json());
-const path = require('path');
